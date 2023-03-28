cd7c3c895d4101388bcfb77178b593b758d46c14
LeHoang <lehoang999113@gmail.com>
2023-03-28 10:57:57 +0700

    update

--- javascript/user/register.js
+++ javascript/user/register.js
@@ -78,7 +78,7 @@ function clickRegister() {
             console.log("Đăng ký thành công");
             swal("Tạo tài khoản thành công", "Hãy đăng nhập để sử dụng dịch vụ của chúng tôi", "success")
                 .then(() => {
-                    window.location.href = "loginForm.html"
+                    window.location.href = "index.html"
                 })
         } else {
             listUserRegister = JSON.parse(listUserRegister);
@@ -95,7 +95,7 @@ function clickRegister() {
                 console.log("Đăng ký thành công");
                 swal("Tạo tài khoản thành công", "Hãy đăng nhập để sử dụng dịch vụ của chúng tôi", "success")
                     .then(() => {
-                        window.location.href = "loginForm.html"
+                        window.location.href = "index.html"
                     })
             } else {
                 console.log("Trùng email");
